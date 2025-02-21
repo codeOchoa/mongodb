07e989f430c10adaf72b308910eb541bda112eb9
Pablo Matias Ochoa <158297956+codeOchoa@users.noreply.github.com>
2025-02-21 08:36:55 -0300

    Se agrega interfaz y arreglos menores

--- routes/views.js
+++ routes/views.js
@@ -5,14 +5,14 @@ const router = express.Router();
 
 // Home static
 router.get('/', async (req, res) => {
-  const products = await readJSONFile(PRODUCTS_FILE);
-  res.render('home', { title: 'Home', products });
+    const products = await readJSONFile(PRODUCTS_FILE);
+    res.render('home', { title: 'Home', products });
 });
 
 // Real time products refresh
-router.get('/realtimeproducts', async (req, res) => {
-  const products = await readJSONFile(PRODUCTS_FILE);
-  res.render('realTimeProducts', { title: 'Products in real time', products });
+router.get('/management', async (req, res) => {
+    const products = await readJSONFile(PRODUCTS_FILE);
+    res.render('realTimeProducts', { title: 'Management', products });
 });
 
 export default router;
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import { createServer } from 'http';
 import { Server } from 'socket.io';
 import { engine } from 'express-handlebars';
 import productsRouter from './routes/products.js';
-import cartsRouter from './routes/cart.js';
+// import cartsRouter from './routes/cart.js';
 import viewsRouter from './routes/views.js';
 import { readJSONFile, writeJSONFile, PRODUCTS_FILE } from './utils/fileManager.js';
 
@@ -24,32 +24,32 @@ app.use(express.json());
 
 // Routes
 app.use('/api/products', productsRouter);
-app.use('/api/carts', cartsRouter);
+// app.use('/api/carts', cartsRouter);
 app.use('/', viewsRouter);
 
 // WebSocket
 io.on('connection', async (socket) => {
-  console.log('🟢 Client connected');
+    console.log('🟢 Client connected');
 
-  const products = await readJSONFile(PRODUCTS_FILE);
-  socket.emit('updateProducts', products);
-
-  socket.on('newProduct', async (product) => {
     const products = await readJSONFile(PRODUCTS_FILE);
-    product.id = String(products.length + 1);
-    products.push(product);
-    await writeJSONFile(PRODUCTS_FILE, products);
-    io.emit('updateProducts', products);
-  });
+    socket.emit('updateProducts', products);
 
-  socket.on('deleteProduct', async (productId) => {
-    let products = await readJSONFile(PRODUCTS_FILE);
-    products = products.filter((p) => p.id !== productId);
-    await writeJSONFile(PRODUCTS_FILE, products);
-    io.emit('updateProducts', products);
-  });
+    socket.on('newProduct', async (product) => {
+        const products = await readJSONFile(PRODUCTS_FILE);
+        product.id = String(products.length + 1);
+        products.push(product);
+        await writeJSONFile(PRODUCTS_FILE, products);
+        io.emit('updateProducts', products);
+    });
+
+    socket.on('deleteProduct', async (productId) => {
+        let products = await readJSONFile(PRODUCTS_FILE);
+        products = products.filter((p) => p.id !== productId);
+        await writeJSONFile(PRODUCTS_FILE, products);
+        io.emit('updateProducts', products);
+    });
 });
 
 server.listen(PORT, () => {
-  console.log('🔥 Server listening on http://localhost:${PORT}');
+    console.log('🔥 Server listening on http://localhost:${PORT}');
 });
